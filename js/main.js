0f30d042e5309e594aae99bb706f18812b6059c7
Curtis Lusmore <curtis@lusmo.re>
2019-01-25 23:48:23 +1100

    Dark mode!

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ document
       navigator.clipboard.writeText(event.target.innerText);
     };
   });
+
+const switchTheme = (function () {
+  let darkTheme = localStorage.getItem('dark') === 'true';
+  const updateTheme = function () {
+    document.body.className = darkTheme ? 'dark' : 'light';
+  };
+  updateTheme();
+  const switchTheme = function () {
+    darkTheme = !darkTheme;
+    localStorage.setItem('dark', darkTheme);
+    updateTheme();
+  };
+  return switchTheme;
+}());
