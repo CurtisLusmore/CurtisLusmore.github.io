bc18ccf4fc7e87b867f953df42fb7b2184881bf3
Curtis Lusmore <curtis@lusmo.re>
2019-02-06 11:42:53 +1100

    Hide Theme button when JS is disabled

--- js/main.js
+++ js/main.js
@@ -8,17 +8,19 @@ document
     };
   });
 
-const switchTheme = (function () {
+(function () {
   const themes = [ 'light', 'dark', 'markdown' ];
   let theme = localStorage.getItem('theme') || 'light';
   const updateTheme = function () {
     document.body.className = theme;
   };
   updateTheme();
-  const switchTheme = function () {
+
+  const themeSwitch = document.getElementById('theme-switch');
+  themeSwitch.style.display = null;
+  themeSwitch.onclick = function () {
     theme = themes[(themes.indexOf(theme) + 1) % themes.length];
     localStorage.setItem('theme', theme);
     updateTheme();
   };
-  return switchTheme;
 }());
