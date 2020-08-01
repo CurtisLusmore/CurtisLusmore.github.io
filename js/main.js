c6327ef5a7d4d5d35444fddc134c2357b0d79b40
Curtis Lusmore <curtis@lusmo.re>
2020-08-01 23:06:45 +1000

    Update logging

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 (function () {
   if (navigator.doNotTrack === '1' || localStorage.getItem('dnt')) return;
   const payload = {
-    location: document.location.pathname,
+    pathname: document.location.pathname,
     referrer: document.referrer
   };
   navigator.sendBeacon(
-    'https://log.lusmo.re/api/log',
+    'https://log.lusmo.re/api/post',
     JSON.stringify(payload)
   );
 }());
